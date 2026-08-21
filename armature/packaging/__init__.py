2161a2dcaba48a8d1fbcd9cd5221c0112e21b6ed
Bryan Sparks <bryan@drycanyon.com>
2026-08-21 15:35:27 -0600

    feat(packaging): package manifest Pydantic models

---
+++ armature/packaging/__init__.py
@@ -0,0 +1,8 @@
+from armature.packaging.manifest import (
+    PACKAGE_API_VERSION, SecretRequirement, SecretsFile, ArtifactSpec, Destinations,
+    PackageManifest, ArtifactResult, TraceRef, ResultsManifest,
+)
+__all__ = [
+    "PACKAGE_API_VERSION", "SecretRequirement", "SecretsFile", "ArtifactSpec",
+    "Destinations", "PackageManifest", "ArtifactResult", "TraceRef", "ResultsManifest",
+]
